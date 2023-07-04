6a1a53ebcfa762a1ebbaba532c842879e185da23
Vlada Falafivka <114653214+CoupleOfMistakes@users.noreply.github.com>
2023-07-04 17:48:30 +0300

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,52 +1,60 @@
 require_relative "validation"
+require_relative "consoleInput"
 
 class Person
-  include Validation
-  attr_accessor :name, :id, :birth_date
+    include Validation
+    include ConsoleInput
+    attr_accessor :name, :id, :birth_date
 
-  def initialize(full_name, id, birth_date)
-    @name = full_name
-    @id = id
-    @birth_date = birth_date
-  end
+    def initialize(full_name, id, birth_date)
+        set_name(full_name)
+        set_id(id)
+        set_birthDate(birth_date)
+    end
 
-  def set_name(full_name)
-    puts ("Setting a new name...")
-    @name = full_name
-  end
+    def set_name(full_name)
+        puts ("Setting a name...")
+        if valid_name?(full_name)
+            @name = full_name
+        else
+            puts "Invalid name"
+            @name = get_input_name()
+        end
+    end
 
-  def set_id(id)
-    puts ("Setting a new id...")
-    @id = id
-  end
+    def set_id(id)
+        puts ("Setting an id...")
+        if valid_inn?(id)
+            @id = id
+        else
+            puts "Invalid id"
+            @id = get_input_id()
+        end
+    end
 
-  def set_birthDate(birth_date)
-    puts ("Setting a new date...")
-    @birth_date = birth_date
-  end
+    def set_birthDate(birth_date)
+        puts ("Setting a date...")
+        if valid_date_format?(birth_date)
+            @birth_date = Date.parse(birth_date)  
+        else
+            puts "Invalid date format"
+            @birth_date = get_input_date()
+        end
+    end
 
-  def info
-    puts ("All the info about the person:\nFull name: #{name}\nid: #{id}\nBirth date: #{birth_date}\n")
-  end
+    def info
+        puts ("All the info about the person:\nFull name: #{name}\nid: #{id}\nBirth date: #{birth_date}\n")
+    end
 
-  def validation
-    puts ("isName: #{Validation.valid_name?(name)}\nisID: #{Validation.valid_inn?(id)}\nisBirthDate: #{Validation.after_date?(birth_date)}")
-    puts ("Validation passed: #{Validation.valid_name?(name) && Validation.valid_inn?(id) && Validation.after_date?(birth_date)}\n\n")
-  end
+    def validation
+        puts ("isName: #{valid_name?(name)}\nisID: #{valid_inn?(id)}\nisBirthDate: #{after_date?(birth_date)}")
+        puts ("Validation passed: #{valid_name?(name) && valid_inn?(id) && after_date?(birth_date)}\n\n")
+    end
 end
 
-person_1 = Person.new("Name Ex", "id32423422356", Date.parse("2000-01-01"))
+=begin 
+person_1 = Person.new("Name Ex", "id32423422356", "2000-01-01")
 person_1.info
 person_1.validation
-person_2 = Person.new("2424", "kinda of id", Date.parse("2023-08-02"))
-person_2.info
-person_2.validation
-person_3 = Person.new("2424", "id234234", Date.parse("2023-03-02"))
-person_3.info
-person_3.validation
-
-person_3.set_name("Anna Kert")
-person_3.info
-person_3.validation
-
-sleep(1000)
+sleep (1000)
+=end 
